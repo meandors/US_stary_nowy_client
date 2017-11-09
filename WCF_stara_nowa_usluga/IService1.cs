afadd971e6dfe2c2625c5a22e4d33735b3831c67
DESKTOP-E1IV82D\meand <meandors@tlen.pl>
2017-11-09 17:58:04 +0100

    Add project files.

---
+++ WCF_stara_nowa_usluga/IService1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCF_stara_nowa_usluga
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IUsluga
+    {
+
+        [OperationContract]
+        Client[] GetClients();
+        [OperationContract]
+        Client GetClient(int id);
+        [OperationContract]
+        void AddClient(Client client);
+        [OperationContract]
+        void ModifyClient(Client client);
+    }
+
+    [DataContract]
+    public class Client
+    {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public string Imie { get; set; }
+        [DataMember]
+        public string Nazwisko { get; set; }
+    }
+}
---
+++ WCF_stara_nowa_usluga/Service1.svc.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCF_stara_nowa_usluga
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Usluga : IUsluga
+    {
+
+        List<Client> listOfClients = new List<Client>();
+        public void AddClient(Client client)
+        {
+            client.Id = listOfClients.Count;
+            listOfClients.Add(client);
+        }
+
+        public Client GetClient(int id)
+        {
+            return listOfClients.ElementAt(id);
+        }
+
+        public Client[] GetClients()
+        {
+            return listOfClients.ToArray();
+        }
+
+        public void ModifyClient(Client client)
+        {
+            throw new FaultException();
+        }
+    }
+}
